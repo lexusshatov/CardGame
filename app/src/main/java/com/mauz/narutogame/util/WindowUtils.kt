114fe856e44bb96fc9649594a0624844371ae2b5
Oleh Shatov <lexusshatov2@gmail.com>
2022-02-08 21:10:27 +0200

    Fix bottom navigation not shows

--- app/src/main/java/com/mauz/narutogame/util/WindowUtils.kt
+++ app/src/main/java/com/mauz/narutogame/util/WindowUtils.kt
@@ -6,7 +6,6 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
 fun Window.hideSystemUI() {
-    WindowCompat.setDecorFitsSystemWindows(this, false)
     WindowInsetsControllerCompat(this, decorView).let { controller ->
         controller.hide(WindowInsetsCompat.Type.systemBars())
         controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
